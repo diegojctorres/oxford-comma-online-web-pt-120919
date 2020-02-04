ccdc1e416db2554d8797b54246ea5b357be6b1f9
diegojctorres <diegojctorres@gmail.com>
2020-02-04 00:44:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length == 4
+  if array.length == 2
     return "#{array[0]} and #{array[1]}"
-  elsif 4 < array.length
+  elsif 2 < array.length
     array[-1].insert(0, "and ")
   end
   array.join(", ")    
